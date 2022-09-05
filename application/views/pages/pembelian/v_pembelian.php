22e5bfdf22226931b6fac6fb180e7770609c7965
mikhaelmm <maramis.mikhael@gmail.com>
2022-09-05 20:37:27 +0800

    pemb

--- application/views/pages/pembelian/v_pembelian.php
+++ application/views/pages/pembelian/v_pembelian.php
@@ -1,2 +1,88 @@
-<div class="display-4 text-center">Halaman Pembelian</div>
-<div class="text-center">Konten disini.</div>
+<div class='row mb-3'>
+		<div class="mr-1" col='3'>
+			<div class='button-pembelian'>
+				<button class="btn btn-light btn-md mr-1" data-toggle="modal" data-target="#">
+                Beli
+            	</button>
+            </div>
+		</div>
+		<div class="mr-1" col='3'>
+			<div class='button-pembelian'>
+				<button class="btn btn-light btn-md mr-1" data-toggle="modal" data-target="#">
+                Barang
+            	</button>
+            </div>
+		</div>
+		<div class="mr-1" col='3'>
+			<div class='button-pembelian'>
+				<button class="btn btn-light btn-md mr-1" data-toggle="modal" data-target="#">
+                Toko
+            	</button>
+            </div>
+		</div>
+	</div>
+
+	<div class='row'>
+		<div class="12">
+		<div class="card">
+			<form>
+			<div class="form-group row">
+				<label for="staticEmail" class="col-sm-2 col-form-label">Email</label>
+				<div class="col-sm-10">
+				<input type="text" readonly class="form-control-plaintext" id="staticEmail" value="email@example.com">
+				</div>
+			</div>
+			<div class="form-group row">
+				<label for="inputPassword" class="col-sm-2 col-form-label">Password</label>
+				<div class="col-sm-10">
+				<input type="password" class="form-control" id="inputPassword" placeholder="Password">
+				</div>
+			</div>
+			</form>
+        </div>
+		</div>
+	</div>
+	
+
+	<div class='row'>
+	<div class="card">
+            <div class="card-header">
+                <h3 class="card-title">Form Input Pembelian</h3>
+			</div>
+            <div class="card-body">
+                <div class="table-responsive">
+                    <table class="table table-bordered table-hover text-sm">
+                        <thead>
+                            <tr>
+                                <th>Nama Barang</th>
+                                <th>Harga Beli</th>
+                                <th>Harga Jual</th>
+                                <th>Tahun</th>
+                                <th>ISBN</th>
+                                <th>Aksi</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>1.</td>
+                                <td>Monitoring Perkuliahan</td>
+                                <td>Alwin Sambul</td>
+                                <td>Erlangga</td>
+                                <td>2021</td>
+                                <td>12111</td>
+                                <td>
+                                    <button type="button" class="btn btn-warning"><i class="fa fa-edit"></i></button>
+                                    <button type="button" class="btn btn-danger"><i class="fa fa-trash"></i></button>
+                                    <button type="button" class="btn bg-purple margin"><i
+                                            class="fa fa-users"></i></button>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+            <!-- <div class="card-footer">
+                Footer
+            </div> -->
+        </div>
+	</div>
